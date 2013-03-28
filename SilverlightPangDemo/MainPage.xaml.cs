4d67a930ed444cca1859d27e9c52ccf903ed81d2
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2013-03-28 23:58:53 +0000

    added project for demo

---
+++ SilverlightPangDemo/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightPangDemo
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();                        
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Execute_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
